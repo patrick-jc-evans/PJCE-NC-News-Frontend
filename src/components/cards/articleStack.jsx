bf60fc5a7ad7f17c2f47ab724f88ebf3843df83c
Patrick-jc-Evans <paddy@paddyevans.com>
2025-02-20 10:18:40 +0000

    add sort by topic in articleStack

--- src/components/cards/articleStack.jsx
+++ src/components/cards/articleStack.jsx
@@ -3,27 +3,31 @@ import axios from "axios"
 import { apiAddress } from "../../../config"
 import { useEffect, useState } from "react"
 
-function ArticleStack({ page }) {
+function ArticleStack({ page, topic }) {
     const [articles, setArticles] = useState([])
 
+    let fetchURL
+    if (topic !== "All Articles")
+        fetchURL = `${apiAddress}/articles?topic=${topic}&p=${page}&limit=10&sort_by=created_at`
+    else
+        fetchURL = `${apiAddress}/articles?p=${page}&limit=10&sort_by=created_at`
+
     function getArticles() {
-        axios
-            .get(`${apiAddress}/articles?p=${page}&limit=10&sort_by=created_at`)
-            .then((response) => {
-                const fullArticleData = response.data.articles
+        axios.get(fetchURL).then((response) => {
+            const fullArticleData = response.data.articles
 
-                const cardData = []
-                fullArticleData.forEach((article) => {
-                    cardData.push([
-                        article.article_id,
-                        article.title,
-                        article.author,
-                        article.topic,
-                    ])
-                })
-
-                setArticles(cardData)
+            const cardData = []
+            fullArticleData.forEach((article) => {
+                cardData.push([
+                    article.article_id,
+                    article.title,
+                    article.author,
+                    article.topic,
+                ])
             })
+
+            setArticles(cardData)
+        })
     }
 
     useEffect(() => {
