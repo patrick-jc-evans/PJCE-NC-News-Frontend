0eab0e13a8f350beec960c52b241b039e9c9f638
Patrick-jc-Evans <paddy@paddyevans.com>
2025-02-20 16:33:39 +0000

    style sorting buttons

--- src/components/buttons/sortby.jsx
+++ src/components/buttons/sortby.jsx
@@ -3,7 +3,14 @@ import "../components.css"
 import SortButton from "./sortButton"
 
 function SortByDropdown({ sortBy, setSortBy, order, setOrder }) {
-    const sortingParams = ["author", "title", "votes", "comment_count", "topic"]
+    const sortingParams = [
+        "author",
+        "title",
+        "votes",
+        "comment_count",
+        "topic",
+        "created_at",
+    ]
 
     const [dropdownVisible, setDropdownVisible] = useState(false)
 
@@ -25,24 +32,27 @@ function SortByDropdown({ sortBy, setSortBy, order, setOrder }) {
 
     if (dropdownVisible)
         return (
-            <section>
+            <section className="sortby-main-buttons">
                 <button onClick={sortByPress}>Sort By</button>
                 <button onClick={orderPress}>{orderDisplay}</button>
                 <br />
-                {sortingParams.map((param, index) => (
-                    <SortButton
-                        key={index}
-                        label={param}
-                        sortBy={sortBy}
-                        setSortBy={setSortBy}
-                    />
-                ))}
+                <div className="sort-button-area">
+                    {sortingParams.map((param, index) => (
+                        <SortButton
+                            key={index}
+                            label={param}
+                            sortBy={sortBy}
+                            setSortBy={setSortBy}
+                        />
+                    ))}
+                </div>
             </section>
         )
     else
         return (
-            <section>
+            <section className="sortby-main-buttons">
                 <button onClick={sortByPress}>Sort By</button>
+                <button onClick={orderPress}>{orderDisplay}</button>
             </section>
         )
 }
