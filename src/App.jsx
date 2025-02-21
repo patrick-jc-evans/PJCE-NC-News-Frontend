e6f4eb789e7a76237ea4c98d351f17f686042572
Patrick-jc-Evans <paddy@paddyevans.com>
2025-02-21 10:11:39 +0000

    add 404 error page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Footer from "./components/footer"
 import Articles from "./pages/articles/articles"
 import Article from "./pages/article/article"
 import Topics from "./pages/topics/topics"
+import ErrorPage from "./pages/errorPage/errorPage"
 
 function App() {
     return (
@@ -18,6 +19,7 @@ function App() {
                 <Route path="/topics" element={<Topics />} />
                 <Route path="/users" element={<p>users</p>} />
                 <Route path="/article" element={<Article />} />
+                <Route path="*" element={<ErrorPage />} />
             </Routes>
             <Footer />
         </div>
---
+++ src/pages/errorPage/errorPage.jsx
@@ -0,0 +1,11 @@
+import "./errorPage.css"
+
+function ErrorPage() {
+    return (
+        <section className="error-section">
+            <h2 className="error-text">404: Page Not Found.</h2>
+        </section>
+    )
+}
+
+export default ErrorPage
